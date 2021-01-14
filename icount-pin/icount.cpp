cac0f9e42835ab97c640025948faeadc6d7b4cfb
Michael Davies <michaelstoby@gmail.com>
2021-01-14 11:19:54 -0600

    Update icount

--- icount-pin/icount.cpp
+++ icount-pin/icount.cpp
@@ -1,10 +1,13 @@
-#include "pin.H"
+
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <stdint.h>
+#include "locks.h"
+
+#include "pin.H"
 #define PINTOOL
-#include "../../include/perf-hooks.hh"
-using std::cerr;
-using std::endl;
+#include "../include/perf-hooks.hh"
 
 
 // To support multiple level calls to START_ROI, we just increment a counter 
@@ -12,15 +15,32 @@ using std::endl;
 UINT64 record = 0;
 UINT64 roi_counter = 0;
 
+std::vector<bool> active;
+std::vector<UINT64> counters;
+
+uint32_t lock;
+std::ofstream ofs;
+
 VOID HandleMagicOp(THREADID tid, ADDRINT op) {
     switch (op) {
         case MAGIC_OP_ROI_BEGIN:
-            cerr << "ROI_BEGIN" << std::endl;
-            record++;
+            futex_lock(&lock);
+            if (counters.size() <= tid) {
+                counters.resize(tid + 1);
+                active.resize(tid + 1);
+            }
+
+            counters[tid] = 0;
+            active[tid] = true;
+            futex_unlock(&lock);
             break;
         case MAGIC_OP_ROI_END:
-            cerr << "ROI_END" << std::endl;
-            record--;
+            active[tid] = false;
+            ofs << "{"
+                << "thread: " << tid << ", "
+                << "icount: " << counters[tid]
+                << "}" << std::endl;
+            
             break;
 
         default:
@@ -30,56 +50,37 @@ VOID HandleMagicOp(THREADID tid, ADDRINT op) {
 
 
 INT32 Usage() {
-    cerr <<
+    std::cerr <<
         "This tool prints out the number of dynamic instructions executed to stderr.\n"
         "\n";
 
-    cerr << KNOB_BASE::StringKnobSummary();
-
-    cerr << endl;
-
+    std::cerr << KNOB_BASE::StringKnobSummary();
+    std::cerr << std::endl;
     return -1;
 }
 
-VOID docount() {
-    roi_counter++;
+VOID docount(THREADID tid) {
+    if (tid < active.size() && active[tid]) counters[tid]++;
 }
 
 VOID Instruction(INS ins, VOID *v) {
 
     if (INS_IsXchg(ins) && INS_OperandReg(ins, 0) == REG_RCX && INS_OperandReg(ins, 1) == REG_RCX) {
-        //info("Instrumenting magic op");
         INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR) HandleMagicOp, IARG_THREAD_ID, IARG_REG_VALUE, REG_ECX, IARG_END);
     }
 
-    if (record > 0) {
-        INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)docount, IARG_END);
-    }
+    INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)docount, IARG_THREAD_ID, IARG_END);
 }
 
-VOID Fini(INT32 code, VOID *v) {
-    std::ofstream ofs("out.log", std::ios::trunc);
-    ofs <<  "Count " << roi_counter  << endl;
-    
-}
-
-
-int main(int argc, char *argv[])
-{
-    if( PIN_Init(argc,argv) )
-    {
+int main(int argc, char *argv[]) {
+    if (PIN_Init(argc, argv)) {
         return Usage();
     }
-    
-    INS_AddInstrumentFunction(Instruction, 0);
-    PIN_AddFiniFunction(Fini, 0);
 
-    // Never returns
+    futex_init(&lock);
+    ofs.open("stats.yaml", std::ios::trunc);
+    INS_AddInstrumentFunction(Instruction, 0);
+
     PIN_StartProgram();
-    
     return 0;
 }
-
-/* ===================================================================== */
-/* eof */
-/* ===================================================================== */
