b7e3fbcc9b4ad808fe55e63a8b64d7f574313970
Michael Davies <michaelstoby@gmail.com>
2021-02-10 10:36:07 -0600

    Update testapp

--- testapp/app.cc
+++ testapp/app.cc
@@ -1,13 +1,23 @@
 
 #include <iostream>
 #include "pin-perf.hh"
+#include "counter-perf.hh"
 
 
 int main() {
     int a;
     float r = 0;
 
+    PERF_COUNTERS_BEGIN();
     PERF_ROI_BEGIN(0, "rname", "opname");
+    for (a = 0; a < 1000000000; a++) {
+        r += 1.0f;
+    }
+    PERF_ROI_END(0);
+    PERF_COUNTERS_END();
+
+    std::cout << r << std::endl;
+    std::cout << ninst.value << std::endl;
 
     return 0;
 }
